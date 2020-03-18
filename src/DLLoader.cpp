8a009f9f66efbfe32b1176393177365d020aaa12
HeyShafty <nathan.lecorchet@epitech.eu>
2020-03-18 12:46:12 +0100

    On va voir ça

--- src/DLLoader.cpp
+++ src/DLLoader.cpp
@@ -23,6 +23,7 @@ Arcade::DLLoader &Arcade::DLLoader::getInstance(void)
     return Arcade::DLLoader::loaderInstance;
 }
 
+// Il faudrait return un && -> On va voir ça
 std::vector<std::string> Arcade::DLLoader::getLibraries(const std::string &dirPath, std::string fst)
 {
     std::vector<std::string> libsPath;
